d39f036c1520afbbd9aed3c94f243b6c9ba9847e
thanhvc <thanhvc@exoplatform.com>
2014-08-07 14:14:21 +0700

    SOC-4360 | [MongoDB] Implement the ActivityStorage plugin mechanism for Mongodb

--- component/core/src/main/java/org/exoplatform/social/core/storage/impl/AbstractStorage.java
+++ component/core/src/main/java/org/exoplatform/social/core/storage/impl/AbstractStorage.java
@@ -27,6 +27,7 @@ import org.chromattic.api.ChromatticSession;
 import org.chromattic.api.Status;
 import org.exoplatform.commons.chromattic.ChromatticManager;
 import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.component.BaseComponentPlugin;
 import org.exoplatform.services.jcr.impl.core.query.QueryImpl;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -40,7 +41,7 @@ import org.exoplatform.social.core.storage.exception.NodeNotFoundException;
  * @author <a href="mailto:alain.defrance@exoplatform.com">Alain Defrance</a>
  * @version $Revision$
  */
-public abstract class AbstractStorage {
+public abstract class AbstractStorage extends BaseComponentPlugin {
   
   private static final Log LOG = ExoLogger.getLogger(AbstractStorage.class);
 
