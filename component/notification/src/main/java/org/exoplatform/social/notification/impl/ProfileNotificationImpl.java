822b6852eeedaf586ec2b940bc2c3f9500f19fff
binhdv <binhdv@exoplatform.com>
2015-03-02 10:18:54 +0700

    SOC-4714 | [User Profile] Make changes on Activity type

--- component/notification/src/main/java/org/exoplatform/social/notification/impl/ProfileNotificationImpl.java
+++ component/notification/src/main/java/org/exoplatform/social/notification/impl/ProfileNotificationImpl.java
@@ -63,4 +63,9 @@ public class ProfileNotificationImpl extends ProfileListenerPlugin {
     ctx.getNotificationExecutor().with(ctx.makeCommand(NotificationKey.key(NewUserPlugin.ID))).execute(ctx);
   }
 
+  @Override
+  public void aboutMeUpdated(ProfileLifeCycleEvent event) {
+    
+  }
+
 }
